8ce932f257c07d95fb17669dbca601793766702d
marciamillanao <marcia.millanao@gmail.com>
2020-08-19 08:34:04 -0400

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyDT2e_bk7zf_ehC9BiLvwdcDdATGHcvXno",
+    authDomain: "burger-queen-28f68.firebaseapp.com",
+    databaseURL: "https://burger-queen-28f68.firebaseio.com",
+    projectId: "burger-queen-28f68",
+    storageBucket: "burger-queen-28f68.appspot.com",
+    messagingSenderId: "504944122027",
+    appId: "1:504944122027:web:5324582841d96389d6bfee",
+    measurementId: "G-SQTXHRHT4E"
+}
